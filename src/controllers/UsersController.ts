931e042e73d9ce89491ba5cb6602528f33a9a1af
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-06-04 09:20:09 -0300

    Correções no crud de usuário
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 import knex from '../database/connection';
-import User from '../models/user';
+import crypto from 'crypto';
 import IUser from '../models/user.interface';
 
 export default class UsersController {
   async index(request: Request, response: Response) {
-    const users = await knex<User>('users')
+    const users = await knex<IUser>('users')
       .select('*')
       .where('removed', false);
     return response.json(users);
@@ -13,7 +13,7 @@ export default class UsersController {
   async show(request: Request, response: Response) {
     try {
       const { id } = request.params;
-      const user = await knex<User>('users')
+      const user = await knex<IUser>('users')
         .where('id', id)
         .where('removed', false)
         .first();
@@ -37,29 +37,38 @@ export default class UsersController {
       image_url,
       password
     } = request.body;
-    const hasUser = await knex<User>('users')
+    const hasUser = await knex<IUser>('users')
       .where('email', email)
-      .where('removed', false)
       .first();
 
-    if (hasUser) {
+    if (hasUser && !hasUser.removed) {
       return response
         .status(400)
         .json({ message: 'User with email already exists' });
     }
     const trx = await knex.transaction();
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hash = crypto.pbkdf2Sync(password, salt,
+      1000, 512, 'sha512').toString('hex');
     try {
-      const user = new User(
-        id,
+      const user = {
+        id: hasUser ? hasUser.id : 0,
         name,
         email,
         image_url,
-      );
-      user.setPassword(password);
-      const [idInserted] = await trx('users').insert(user);
-    
+        hash,
+        salt,
+        removed: false,
+        created_at: hasUser ? hasUser.created_at : new Date(),
+        updated_at: new Date(),
+      } as IUser;
+      if (user.id !== 0) {
+        await trx('users').update(user).where('id', user.id);
+      } else {
+        const [idInserted] = await trx('users').insert(user);
+        user.id = idInserted;
+      }
       await trx.commit();
-      user.id = idInserted;
       return response.json(user);
     } catch (error) {
       await trx.rollback();
@@ -73,26 +82,21 @@ export default class UsersController {
     const { id } = request.params;
     const {
       name,
-      password,
       image_url,
     } = request.body;
-    const userOld = await knex<User>('users')
+    const user = await knex<IUser>('users')
       .where('id', id)
       .where('removed', false)
       .first();
 
-    if (!userOld) {
+    if (!user) {
       return response.status(400).json({ message: 'User not found' });
     }
     const trx = await knex.transaction();
     try {
-      const user = new User(
-        userOld.id,
-        name,
-        userOld.email,
-        image_url
-      );
-      user.setPassword(password);
+      user.name = name;
+      user.image_url = image_url;
+      user.updated_at = new Date();
       await trx('users')
         .update(user)
         .where('id', user.id);
@@ -121,7 +125,7 @@ export default class UsersController {
     try {
       user.removed = true;
       user.updated_at = new Date();
-      await trx<User>('users')
+      await trx<IUser>('users')
         .update(user)
         .where('id', user.id);
       
--- src/models/user.interface.ts
+++ src/models/user.interface.ts
@@ -1,16 +1,11 @@
 export default interface IUser {
-  id: number,
-  name: string,
-  email: string,
-  image_url: string,
+  id: number;
+  name: string;
+  email: string;
+  image_url: string;
   hash: string;
   salt: string;
-  created_at?: Date;
-  updated_at?: Date;
   removed: boolean;
-
-  update(name: string, image_url: string, password: string): void;
-  delete(): void;
-  setPassword(password: string): void;
-  validatePassword(password: string): boolean;
+  created_at: Date;
+  updated_at: Date;
 }
